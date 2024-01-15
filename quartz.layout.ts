57643d268441815d50d585c6413da20cbea614a7
infoseek <infoseek@163.com>
2024-01-15 23:54:25 +0800

    vault backup: 2024-01-15 23:54:25
    Affected files:
    quartz.layout.ts
    quartz/cfg.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,6 +14,7 @@ export const sharedPageComponents: SharedLayout = {
 }
 
 // components for pages that display a single page (e.g. a single note)
+// @ts-ignore
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
