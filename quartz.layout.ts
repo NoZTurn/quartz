d88a3942c854b3ad46249fb08fb5d8c536605669
infoseek <infoseek@163.com>
2024-10-04 00:20:38 +0800

    vault backup from laptop: 2024-10-04 00:20:38
    Affected files:
    quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,9 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Giscus(),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -33,7 +35,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.Giscus(),
   ],
 }
 
