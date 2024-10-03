a491b6562e458982c33e68194d4dcc42a8ed47b8
infoseek <infoseek@163.com>
2024-10-03 23:52:54 +0800

    vault backup from laptop: 2024-10-03 23:52:54
    Affected files:
    quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,7 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.DesktopOnly(Component.Giscus()),
+    Component.Giscus(),
   ],
 }
 
